75f64b9150484ee91dbff2dedbc2d8c18d8bc9f0
Andy Blackburn <andyblac@o2.co.uk>
2012-06-29 23:46:01 +0100

    VentonFancontrol: fox translation support.

--- VentonFancontrol/src/plugin.py
+++ VentonFancontrol/src/plugin.py
@@ -1,3 +1,6 @@
+# for localized messages
+from . import _
+
 from Screens.Screen import Screen
 from Components.ConfigList import ConfigListScreen
 from Components.config import config, ConfigSubsection, ConfigInteger, ConfigSelection, getConfigListEntry
