87de8022821ab71650465723e3b4c85ecae59cd5
Andy Blackburn <andrew.blackburn644@btinternet.com>
2012-07-13 02:39:01 +0100

    translations: fix code.

--- TmTwinFanControl/src/__init__.py
+++ TmTwinFanControl/src/__init__.py
@@ -4,20 +4,19 @@ from Tools.Directories import resolveFilename, SCOPE_PLUGINS, SCOPE_LANGUAGE
 import os,gettext
 
 PluginLanguageDomain = "FanControl"
-PluginLanguagePath = "SystemPlugins/FanControl/local"
+PluginLanguagePath = "SystemPlugins/FanControl/locale"
 
 def localeInit():
 	lang = language.getLanguage()[:2] # getLanguage returns e.g. "fi_FI" for "language_country"
 	os.environ["LANGUAGE"] = lang # Enigma doesn't set this (or LC_ALL, LC_MESSAGES, LANG). gettext needs it!
-#	print "[ViX] set language to ", lang
+# 	print "["+PluginLanguageDomain+"] set language to ", lang
 	gettext.bindtextdomain(PluginLanguageDomain, resolveFilename(SCOPE_PLUGINS, PluginLanguagePath))
-	gettext.bindtextdomain('enigma2', resolveFilename(SCOPE_LANGUAGE, ""))
 
 def _(txt):
 	t = gettext.dgettext(PluginLanguageDomain, txt)
 	if t == txt:
-		#print "[CrossEPG] fallback to default translation for", txt
-		t = gettext.dgettext('enigma2', txt)
+# 		print "["+PluginLanguageDomain+"] fallback to default translation for", txt
+		t = txt
 	return t
 
 
