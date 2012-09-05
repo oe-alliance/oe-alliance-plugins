71097b20a1b9e60a3b454e9c22b6f971882b5b6a
Andy Blackburn <andrew.blackburn644@btinternet.com>
2012-09-05 18:30:57 +0100

    [AutoBouquets] fix translation support.

--- AutoBouquets/src/__init__.py
+++ AutoBouquets/src/__init__.py
@@ -4,7 +4,7 @@ from Tools.Directories import resolveFilename, SCOPE_PLUGINS, SCOPE_LANGUAGE
 import os,gettext
 
 PluginLanguageDomain = "AutoBouquets"
-PluginLanguagePath = "SystemPlugins/AutoBouquets/locale"
+PluginLanguagePath = "Extensions/AutoBouquets/locale"
 
 def localeInit():
 	lang = language.getLanguage()[:2] # getLanguage returns e.g. "fi_FI" for "language_country"
