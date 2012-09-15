60ba7b991fdea01363b3971d1705bb7578fc8611
Captain <captain.onboard@web.de>
2012-09-16 00:04:57 +0200

    update xtrendremote to version 1.3

--- XtrendRemote/src/plugin.py
+++ XtrendRemote/src/plugin.py
@@ -1,4 +1,4 @@
-#  ET-View RCSetup version 1.2
+#  ET-View RCSetup version 1.3
 #  Remote Controller Setup (RCSetup)
 # -*- coding: utf-8 -*-
 # for localized messages
@@ -9,7 +9,7 @@ from Components.ConfigList import ConfigListScreen
 from Components.config import config, ConfigSubsection, ConfigInteger, ConfigSelection, ConfigSlider, getConfigListEntry
 from os import path as os_path, chmod as os_chmod, unlink as os_unlink, system as os_system
 
-modelist = {"5": _("ET9000/ET9100"), "4": _("DMM/DMM ADV"), "7": _("ET5000/ET6000"), "9": _("ET9500"), "11": _("ET9200/ET9500") }
+modelist = {"5": _("ET9000/ET9100"), "4": _("DMM/DMM ADV"), "7": _("ET5000/ET6000"), "9": _("ET6500/ET9500"), "11": _("ET9200/ET9500"), "13": _("ET4000") }
 
 config.plugins.RCSetup = ConfigSubsection()
 from os import system as os_system
@@ -17,7 +17,9 @@ file = open("/proc/stb/ir/rc/type", "r")
 text=file.read()
 file.close()
 temp = int(text)
-if temp == 11:
+if temp == 13:
+	config.plugins.RCSetup.mode = ConfigSelection(choices = modelist, default = "13")
+elif temp == 11:
 	config.plugins.RCSetup.mode = ConfigSelection(choices = modelist, default = "11")
 elif temp == 9:
 	config.plugins.RCSetup.mode = ConfigSelection(choices = modelist, default = "9")
@@ -100,12 +102,14 @@ class RCSetupScreen(Screen, ConfigListScreen):
 			self.removeFile()
 		elif tmp == 4:
 			self.createFile()
-		elif tmp == 6:
-			self.createFile()
 		elif tmp == 7:
 			self.createFile()
+		elif tmp == 9:
+			self.createFile()	
 		elif tmp == 11:
 			self.createFile()
+		elif tmp == 13:
+			self.createFile()	
 
 	def createFile(self):
 		file = open("/etc/rc3.d/S30rcsetup", "w")
