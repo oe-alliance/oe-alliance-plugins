4fb91f92affc40822b4815252fb330313724bb43
Rob van der Does <robvddoes@gmail.com>
2012-11-30 15:40:18 +0100

    [VuUnoFanControl]  added temp & RPM

--- VuUnoFanControl/src/plugin.py
+++ VuUnoFanControl/src/plugin.py
@@ -1,151 +1,116 @@
 # for localized messages
 from . import _
 
-from Screens.Screen import Screen
-from Components.ConfigList import ConfigListScreen
-from Components.config import config, getConfigListEntry, ConfigSubsection, ConfigSelection, ConfigInteger
-from Components.ActionMap import ActionMap,NumberActionMap
-from Screens.MessageBox import MessageBox
+from Components.ActionMap import ActionMap
+from Components.Sensors import sensors
+from Components.Sources.Sensor import SensorSource
 from Components.Sources.StaticText import StaticText
-from Plugins.Plugin import PluginDescriptor
-from Plugins.SystemPlugins.FanControl.InstandbyOn import instandbyon
-import NavigationInstance
-from enigma import eTimer
+from Components.ConfigList import ConfigListScreen
+from Components.config import getConfigListEntry
 
-class ManualFancontrol(Screen,ConfigListScreen):
+from Screens.Screen import Screen
+
+from Plugins.Plugin import PluginDescriptor
+from Components.FanControl import fancontrol
+
+class ManualFancontrol(Screen, ConfigListScreen):
 	skin = """
-			<screen name="ManualFancontrol" position="center,center" size="560,300" >
-			<widget name="config" zPosition="2" position="5,50" size="550,200" scrollbarMode="showOnDemand" transparent="1" />
-			<widget source="current" render="Label" position="150,270" zPosition="1" size="280,30" font="Regular;18" halign="left" valign="center" transparent="1" />
+		<screen position="center,center" size="570,420" title="FanControl settings" >
 			<ePixmap pixmap="skin_default/buttons/red.png" position="0,0" size="140,40" alphatest="on" />
 			<ePixmap pixmap="skin_default/buttons/green.png" position="140,0" size="140,40" alphatest="on" />
-			<widget source="key_red" render="Label" position="0,0" zPosition="1" size="140,40" font="Regular;20" halign="center" valign="center" backgroundColor="#9f1313" transparent="1" />
-			<widget source="key_green" render="Label" position="140,0" zPosition="1" size="140,40" font="Regular;20" halign="center" valign="center" backgroundColor="#1f771f" transparent="1" />
-			</screen>"""
 
-	def __init__(self,session):
-		Screen.__init__(self,session)
-		self.session = session
+
+			<widget source="red" render="Label" position="0,0" zPosition="1" size="140,40" font="Regular;20" halign="center" valign="center" backgroundColor="#9f1313" transparent="1" />
+			<widget source="green" render="Label" position="140,0" zPosition="1" size="140,40" font="Regular;20" halign="center" valign="center" backgroundColor="#1f771f" transparent="1" />
+			<widget source="yellow" render="Label" position="280,0" zPosition="1" size="140,40" font="Regular;20" halign="center" valign="center" backgroundColor="#a08500" transparent="1" />
+			<widget source="blue" render="Label" position="420,0" zPosition="1" size="140,40" font="Regular;20" halign="center" valign="center" backgroundColor="#18188b" transparent="1" />
+			
+			<widget name="config" position="10,50" size="550,120" scrollbarMode="showOnDemand" />
+			
+			<widget source="SensorTempText0" render="Label" position="10,150" zPosition="1" size="90,40" font="Regular;20" halign="left" valign="top" backgroundColor="#9f1313" transparent="1" />
+			<widget source="SensorTemp0" render="Label" position="100,150" zPosition="1" size="100,20" font="Regular;19" halign="right">
+				<convert type="SensorToText"></convert>
+			</widget>
+
+			<widget source="SensorFanText0" render="Label" position="290,150" zPosition="1" size="90,40" font="Regular;20" halign="left" valign="top" backgroundColor="#9f1313" transparent="1" />
+			<widget source="SensorFan0" render="Label" position="380,150" zPosition="1" size="150,20" font="Regular;19" halign="right">
+				<convert type="SensorToText"></convert>
+			</widget>
+		</screen>"""
+
+	def __init__(self, session, args = None):
+		Screen.__init__(self, session)
 		Screen.setTitle(self, _("Fan Control"))
-		self["shortcuts"] = ActionMap(["ShortcutActions", "SetupActions" ],
-		{
-			"ok": self.keySave,
-			"cancel": self.keyCancel,
-			"red": self.keyCancel,
-			"green": self.keySave,
-		}, -2)
-		self.list = []
-		ConfigListScreen.__init__(self, self.list,session = self.session)
+		
+		templist = sensors.getSensorsList(sensors.TYPE_TEMPERATURE)
+		tempcount = len(templist)
+		fanlist = sensors.getSensorsList(sensors.TYPE_FAN_RPM)
+		fancount = len(fanlist)
+		
 		self["key_red"] = StaticText(_("Cancel"))
 		self["key_green"] = StaticText(_("Save"))
-		self["current"] = StaticText(" ")
-		self.configSetup()
-		self.oldfanoffmode = instandbyon.fanoffmode
-		if instandbyon.fanoffmode is 'ON' :
-			instandbyon.checkStatusLoopStop()
-		self.checkFanTimer = eTimer()
-		self.checkFanTimer.callback.append(self.fan_pwm_error)
-		self.onLayoutFinish.append(self.checkFan)
 
-	def checkFan(self):
-		if not instandbyon.check_fan_pwm():
-			self.checkFanTimer.start(10,True)
-
-	def fan_pwm_error(self):
-		self.session.openWithCallback(self.close, MessageBox, _("Can not open 'fan_pwm'"), MessageBox.TYPE_ERROR)
-
-	def displayCurrentValue(self):
-		currrent_val = self["config"].getCurrent()[0]+" : "+str(self["config"].getCurrent()[1].value)
-		self["current"].setText(_(currrent_val))
-#		print currrent_val
-
-	def selectionChanged(self):
-		if self["config"].getCurrent() == self.pwmEntry:
-			instandbyon.setPWM(self["config"].getCurrent()[1].value)
-
-	def keyLeft(self):
-		oldpwmvalue=config.plugins.manualfancontrols.pwmvalue.value
-		ConfigListScreen.keyLeft(self)
-		if self["config"].getCurrent() == self.pwmEntry and oldpwmvalue == 5:
-			self.createSetup()
-		else:
-			self.displayCurrentValue()
-		self.selectionChanged()
-
-	def keyRight(self):
-		oldpwmvalue=config.plugins.manualfancontrols.pwmvalue.value
-		ConfigListScreen.keyRight(self)
-		if self["config"].getCurrent() == self.pwmEntry and oldpwmvalue == 0:
-			self.createSetup()
-			while self["config"].getCurrent() != self.pwmEntry:
-				self["config"].setCurrentIndex(self["config"].getCurrentIndex()+1)
-		else:
-			self.displayCurrentValue()
-		self.selectionChanged()
-
-	def createSetup(self):
+		for count in range(8):
+			if count < tempcount:
+				id = templist[count]
+				self["SensorTempText%d" % count] = StaticText(sensors.getSensorName(id))		
+				self["SensorTemp%d" % count] = SensorSource(sensorid = id)
+			else:
+				self["SensorTempText%d" % count] = StaticText("")
+				self["SensorTemp%d" % count] = SensorSource()
+				
+			if count < fancount:
+				id = fanlist[count]
+				self["SensorFanText%d" % count] = StaticText(sensors.getSensorName(id))		
+				self["SensorFan%d" % count] = SensorSource(sensorid = id)
+			else:
+				self["SensorFanText%d" % count] = StaticText("")
+				self["SensorFan%d" % count] = SensorSource()
+		
 		self.list = []
-		if config.plugins.manualfancontrols.pwmvalue.value > 0:
-			self.list.append( self.standbyEntry )
-		self.list.append( self.pwmEntry )
-		self.list.append( self.periodEntry )
-		self["config"].list = self.list
-		self["config"].l.setList(self.list)
+		for count in range(fancontrol.getFanCount()):
+			self.list.append(getConfigListEntry(_("Fan %d Voltage") % (count + 1), fancontrol.getConfig(count).vlt))
+			self.list.append(getConfigListEntry(_("Fan %d PWM") % (count + 1), fancontrol.getConfig(count).pwm))
+			self.list.append(getConfigListEntry(_("Standby Fan %d Voltage") % (count + 1), fancontrol.getConfig(count).vlt_standby))
+			self.list.append(getConfigListEntry(_("Standby Fan %d PWM") % (count + 1), fancontrol.getConfig(count).pwm_standby))
+		
+		ConfigListScreen.__init__(self, self.list, session = self.session)
+		#self["config"].list = self.list
+		#self["config"].setList(self.list)
+		self["config"].l.setSeperation(300)
+		
+		self["actions"] = ActionMap(["OkCancelActions", "ColorActions"], 
+		{
+			"ok": self.save,
+			"cancel": self.revert,
+			"red": self.revert,
+			"green": self.save
+		}, -1)
 
-	def configSetup(self):
-		self.standbyEntry = getConfigListEntry(_("FanOFF InStanby"), config.plugins.manualfancontrols.standbymode)
-		self.pwmEntry = getConfigListEntry(_("PWM value"), config.plugins.manualfancontrols.pwmvalue)
-		self.periodEntry = getConfigListEntry(_("Status Check Period"), config.plugins.manualfancontrols.checkperiod)
-		if not self.displayCurrentValue in self["config"].onSelectionChanged:
-			self["config"].onSelectionChanged.append(self.displayCurrentValue)
-		self.createSetup()
-
-	def keySave(self):
-		if instandbyon.fanoffmode is 'OFF' and config.plugins.manualfancontrols.pwmvalue.value == 0:
-#			print "[ManualFancontrol] instandbyon.fanoffmode 'OFF' -> 'ON'"
-			instandbyon.fanoffmode = 'ON'
-			instandbyon.addRecordEventCB()
-			instandbyon.checkStatusLoopStart()
-		elif instandbyon.fanoffmode is 'ON' and config.plugins.manualfancontrols.pwmvalue.value != 0:
-#			print "[ManualFancontrol] instandbyon.fanoffmode 'ON' -> 'OFF'"
-			instandbyon.fanoffmode = 'OFF'
-			instandbyon.removeRecordEventCB()
-#			instandbyon.checkStatusLoopStop() # stoped at init
-		elif self.oldfanoffmode is 'ON' :
-			instandbyon.checkStatusLoopStart()
-		instandbyon.checkStstus()
-		ConfigListScreen.keySave(self)
-
-	def cancelConfirm(self, result):
-		if not result:
-			return
-		for x in self["config"].list:
-			x[1].cancel()
-		instandbyon.checkStstus()
-		self.oldfanoffmode = instandbyon.fanoffmode
-		if self.oldfanoffmode is 'ON' :
-			instandbyon.checkStatusLoopStart()
+	def save(self):
+		for count in range(fancontrol.getFanCount()):
+			fancontrol.getConfig(count).vlt.save()
+			fancontrol.getConfig(count).pwm.save()
+			fancontrol.getConfig(count).vlt_standby.save()
+			fancontrol.getConfig(count).pwm_standby.save()
 		self.close()
 
-	def keyCancel(self):
-		if self["config"].isChanged():
-			self.session.openWithCallback(self.cancelConfirm, MessageBox, _("Really close without saving settings?"))
-		else:
-			instandbyon.checkStstus()
-			if self.oldfanoffmode is 'ON' :
-				instandbyon.checkStatusLoopStart()
-			self.close()
+	def revert(self):
+		for count in range(fancontrol.getFanCount()):
+			fancontrol.getConfig(count).vlt.load()
+			fancontrol.getConfig(count).pwm.load()
+			fancontrol.getConfig(count).vlt_standby.load()
+			fancontrol.getConfig(count).pwm_standby.load()
+		self.close()
 
 def main(session, **kwargs):
 	session.open(ManualFancontrol)
 
-def selSetup(menuid, **kwargs):
+def startMenu(menuid):
 	if menuid != "system":
-		return [ ]
-	return [(_("Fan Control"), main, "fansetup_config", 70)]
+		return []
+	return [(_("Fan control"), main, "ManualFancontrol", 80)]
 
 def Plugins(**kwargs):
-	from os import path
-	if not path.exists("/usr/lib/enigma2/python/Plugins/Extensions/FanControl2/plugin.pyo"):
-		return [PluginDescriptor(name=_("Fan Control"), description=_("setup Fancontol inStandby mode"), where = PluginDescriptor.WHERE_MENU, needsRestart = True, fnc=selSetup)]
-	return []
+	return PluginDescriptor(name = "Fan control", description = _("Temperature and Fan control"), where = PluginDescriptor.WHERE_MENU, needsRestart = False, fnc = startMenu)
+
