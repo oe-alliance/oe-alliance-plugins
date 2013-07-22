b3bf8e9fe67c018df5d65b02976d9a295f665e92
Andy Blackburn <andrew.blackburn644@btinternet.com>
2013-07-22 08:39:43 +0100

    [HbbTV] strip all none printable charters from data grabbed from stream.

--- HbbTV/src/aitreader.py
+++ HbbTV/src/aitreader.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import os, xml.dom.minidom
+import os, xml.dom.minidom, re
 
 DUMPBIN = "/usr/lib/enigma2/python/Plugins/Extensions/HbbTV/dumpait"
 class eAITSectionReader:
@@ -49,7 +49,10 @@ class eAITSectionReader:
 			return False
 		if len(document) == 0:
 			return False
-		document = document.decode("cp1252").encode("utf-8")
+		# strip all none printable charators from data grabed from stream.
+		control_chars = ''.join(map(unichr, range(0,32) + range(127,160)))
+		control_char_re = re.compile('[%s]' % re.escape(control_chars))
+		document = control_char_re.sub('', document)
 		self.mDocument = xml.dom.minidom.parseString(document)
 		return True
 
