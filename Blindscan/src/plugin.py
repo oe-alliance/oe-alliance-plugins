8853d6cdd5480a73282973d982754c08d1237c07
Andy Blackburn <andrew.blackburn644@btinternet.com>
2013-07-26 16:02:08 +0100

    add some support for 'ini' boxes.

--- Blindscan/src/plugin.py
+++ Blindscan/src/plugin.py
@@ -567,7 +567,7 @@ class Blindscan(ConfigListScreen, Screen):
 			status_box_start_freq = temp_start_int_freq + uni_lnb_loc_osc[band]
 			status_box_end_freq = temp_end_int_freq + uni_lnb_loc_osc[band]
 			
-		if getBoxType().startswith('venton'):
+		if getBoxType().startswith('venton') or getBoxType().startswith('ini'):
 			cmd = "ini_blindscan %d %d %d %d %d %d %d %d" % (temp_start_int_freq, temp_end_int_freq, self.blindscan_start_symbol.value, self.blindscan_stop_symbol.value, tab_pol[pol], tab_hilow[band], self.feid, self.getNimSocket(self.feid)) 
 		elif getBoxType().startswith('vu'):
 			try:
