ddd2d8bbd0e55f46837ec440012e387a8299f2cc
atvcaptain <captain@web.de>
2014-05-15 15:55:27 +0200

    remote control small update for new models

--- XtrendRemote/src/plugin.py
+++ XtrendRemote/src/plugin.py
@@ -9,7 +9,7 @@ from Components.ConfigList import ConfigListScreen
 from Components.config import config, ConfigSubsection, ConfigInteger, ConfigSelection, ConfigSlider, getConfigListEntry
 from os import path as os_path, chmod as os_chmod, unlink as os_unlink, system as os_system
 
-modelist = {"3": _("OdinM9"), "5": _("ET9000/ET9100"), "4": _("DMM/DMM ADV"), "6": _("DMM/DMM ADV"), "7": _("ET5000/ET6000"), "8": _("Vu"), "9": _("ET6500/ET9500"), "11": _("ET9200/ET9500"), "13": _("ET4000"), "14": _("XP1000"), "17": _("HD1100"), "18": _("FORMULER"), "19": _("HD2400") }
+modelist = {"3": _("OdinM9"), "5": _("ET9000/ET9100"), "4": _("DMM/DMM ADV"), "6": _("DMM/DMM ADV"), "7": _("ET5000/ET6000"), "8": _("Vu"), "9": _("ET6500/ET9500"), "11": _("ET9200/ET9500"), "13": _("ET4000"), "14": _("XP1000"), "16": _("HD1100"), "17": _("XP3000"), "18": _("F1/F3"), "19": _("HD2400") }
 
 config.plugins.RCSetup = ConfigSubsection()
 from os import system as os_system
@@ -23,6 +23,8 @@ elif temp == 18:
 	config.plugins.RCSetup.mode = ConfigSelection(choices = modelist, default = "18")
 elif temp == 17:
 	config.plugins.RCSetup.mode = ConfigSelection(choices = modelist, default = "17")
+elif temp == 16:
+	config.plugins.RCSetup.mode = ConfigSelection(choices = modelist, default = "16")
 elif temp == 14:
 	config.plugins.RCSetup.mode = ConfigSelection(choices = modelist, default = "14")
 elif temp == 13:
@@ -131,6 +133,8 @@ class RCSetupScreen(Screen, ConfigListScreen):
 		elif tmp == 13:
 			self.createFile()
 		elif tmp == 14:
+			self.createFile()
+		elif tmp == 16:
 			self.createFile()
 		elif tmp == 17:
 			self.createFile()
