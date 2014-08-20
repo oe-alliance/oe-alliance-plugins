a1060b9478d58128c77376601217201885d14b22
skl <skl@skl.net>
2014-08-20 13:26:08 +0200

    Fix SH4CEControl (again)

--- SH4CEControl/src/__init__.py
+++ SH4CEControl/src/__init__.py
@@ -1,14 +1,1 @@
-from Components.Language import language
-from Tools.Directories import resolveFilename, SCOPE_LANGUAGE, SCOPE_PLUGINS
-import gettext, os
-lang = language.getLanguage()
-os.environ['LANGUAGE'] = lang[:2]
-gettext.bindtextdomain('enigma2', resolveFilename(SCOPE_LANGUAGE))
-gettext.textdomain('enigma2')
-gettext.bindtextdomain('CEControl', '%s%s' % (resolveFilename(SCOPE_PLUGINS), 'Extensions/CEControl/locale/'))
-
-def _(txt):
-    t = gettext.dgettext('CEControl', txt)
-    if t == txt:
-        t = gettext.gettext(txt)
-    return t
+pass
