e672b2fe7c38755620a81da1d8414955934c8dde
atvcaptain <captain@web.de>
2014-09-07 22:41:09 +0200

    [spark] pigulux plus have a vfd stop load vfd plugin

--- SparkVFDControl/src/plugin.py
+++ SparkVFDControl/src/plugin.py
@@ -295,7 +295,7 @@ def sessionstart(reason, **kwargs):
 	controlsparkVfd()
 
 def Plugins(**kwargs):
-		if getBoxType() in ('amiko8900', 'sognorevolution', 'arguspingulux', 'arguspinguluxmini', 'arguspinguluxplus', 'sparkreloaded', 'sabsolo', 'sparklx', 'gis8120'):
+		if getBoxType() in ('amiko8900', 'sognorevolution', 'arguspingulux', 'arguspinguluxmini', 'sparkreloaded', 'sabsolo', 'sparklx', 'gis8120'):
 			return [ PluginDescriptor(where=[PluginDescriptor.WHERE_AUTOSTART, PluginDescriptor.WHERE_SESSIONSTART], fnc=sessionstart),
 				PluginDescriptor(name="LED Display Setup", description="Change VFD display settings",where = PluginDescriptor.WHERE_MENU, fnc = main) ]
 		else:
