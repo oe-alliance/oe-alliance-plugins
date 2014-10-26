779748f3ef7cbfb6b5cb3561430c8a40e6574d91
daTa <data-devil@arcor.de>
2014-10-26 22:54:02 +0100

    [SparkVFDControl] add option red/green LED and show Channelname

--- SparkVFDControl/src/plugin.py
+++ SparkVFDControl/src/plugin.py
@@ -19,10 +19,14 @@ from Tools.Directories import fileExists
 from boxbranding import getBoxType
 
 import Screens.Standby
+import NavigationInstance
+from Screens.SessionGlobals import SessionGlobals
 
 config.plugins.VFD_spark = ConfigSubsection()
-config.plugins.VFD_spark.showClock = ConfigSelection(default = "True_Switch", choices = [("False",_("Channelnumber in Standby off")),("True",_("Channelnumber in Standby Clock")), ("True_Switch",_("Channelnumber/Clock in Standby Clock")),("True_All",_("Clock always")),("Off",_("Always off"))])
+config.plugins.VFD_spark.showClock = ConfigSelection(default = "True_Switch", choices = [("NameOff",_("Channelname in Standby off")), ("NameOn",_("Channelname in Standby Clock")), ("False",_("Channelnumber in Standby off")),("True",_("Channelnumber in Standby Clock")), ("True_Switch",_("Channelnumber/Clock in Standby Clock")),("True_All",_("Clock always")),("Off",_("Always off"))])
 config.plugins.VFD_spark.timeMode = ConfigSelection(default = "24h", choices = [("12h"),("24h")])
+config.plugins.VFD_spark.redLed = ConfigSelection(default = "0", choices = [("0",_("Off")),("1",_("Standby only")), ("2",_("Record only"))])
+config.plugins.VFD_spark.greenLed = ConfigSelection(default = "0", choices = [("0",_("Off")),("1",_("Standby only"))])
 
 def vfd_write(text):
 	open("/dev/dbox/oled0", "w").write(text)
@@ -47,6 +51,7 @@ class Channelnumber:
 			{
 				iPlayableService.evUpdatedEventInfo: self.__eventInfoChanged
 			})
+		session.nav.record_event.append(self.gotRecordEvent)
 
 	def __eventInfoChanged(self):
 		if config.plugins.VFD_spark.showClock.value == 'Off' or config.plugins.VFD_spark.showClock.value == 'True_All':
@@ -56,14 +61,21 @@ class Channelnumber:
 		if info is None:
 			chnr = "----"
 		else:
-			chnr = self.getchannelnr()
+			if config.plugins.VFD_spark.showClock.value == 'NameOff' or config.plugins.VFD_spark.showClock.value == 'NameOn':
+				na = info.getName()
+				name = na.replace('\xc2\x87', '').replace('\xc2\x86', '')
+			else:
+				chnr = self.getchannelnr()
 		info = None
 		service = None
 		if chnr == "----":
 			vfd_write(chnr)
 		else:
-			Channelnr = "%04d" % (int(chnr))
-			vfd_write(Channelnr)
+			if config.plugins.VFD_spark.showClock.value == 'NameOff' or config.plugins.VFD_spark.showClock.value == 'NameOn':
+				vfd_write(name)
+			else:
+				Channelnr = "%04d" % (int(chnr))
+				vfd_write(Channelnr)
 
 	def getchannelnr(self):
 		if InfoBar.instance is None:
@@ -93,7 +105,7 @@ class Channelnumber:
 		return chnr
 
 	def prikaz(self):
-		if config.plugins.VFD_spark.showClock.value == 'True' or config.plugins.VFD_spark.showClock.value == 'True_All' or config.plugins.VFD_spark.showClock.value == 'True_Switch':
+		if config.plugins.VFD_spark.showClock.value == 'True' or config.plugins.VFD_spark.showClock.value == 'NameOn' or config.plugins.VFD_spark.showClock.value == 'True_All' or config.plugins.VFD_spark.showClock.value == 'True_Switch':
 			clock = str(localtime()[3])
 			clock1 = str(localtime()[4])
 			if config.plugins.VFD_spark.timeMode.value != '24h':
@@ -112,7 +124,7 @@ class Channelnumber:
 			vfd_write("....")
 
 	def vrime(self):
-		if (config.plugins.VFD_spark.showClock.value == 'True' or config.plugins.VFD_spark.showClock.value == 'False' or config.plugins.VFD_spark.showClock.value == 'True_Switch') and not Screens.Standby.inStandby:
+		if (config.plugins.VFD_spark.showClock.value == 'True' or config.plugins.VFD_spark.showClock.value == 'False' or config.plugins.VFD_spark.showClock.value == 'NameOff' or config.plugins.VFD_spark.showClock.value == 'NameOn' or config.plugins.VFD_spark.showClock.value == 'True_Switch') and not Screens.Standby.inStandby:
 			if config.plugins.VFD_spark.showClock.value == 'True_Switch':
 				if time() >= self.begin:
 					self.endkeypress = False
@@ -136,6 +148,26 @@ class Channelnumber:
 	def keyPressed(self, key, tag):
 		self.begin = time() + int(self.channelnrdelay)
 		self.endkeypress = True
+
+	def gotRecordEvent(self, service, event):
+		if config.plugins.VFD_spark.redLed.value == '2':
+			self.RecTimer = eTimer()
+			self.RecTimer.callback.append(self.showRec)
+			self.RecTimer.start(1000, True)
+    
+	def showRec(self):
+		recordings = len(NavigationInstance.instance.getRecordings())
+		
+		if recordings >= 1:
+			pattern = 4294967295
+			f = open("/proc/stb/fp/led0_pattern", "w")
+			f.write("%08x" % pattern)
+			f.close()
+		else:
+			pattern = 0
+			f = open("/proc/stb/fp/led0_pattern", "w")
+			f.write("%08x" % pattern)
+			f.close()
 
 ChannelnumberInstance = None
 
@@ -200,6 +232,8 @@ class VFD_SPARKSetup(ConfigListScreen, Screen):
 		self.list.append(getConfigListEntry(_("Show on VFD"), config.plugins.VFD_spark.showClock))
 		if config.plugins.VFD_spark.showClock.value != "Off":
 			self.list.append(getConfigListEntry(_("Time mode"), config.plugins.VFD_spark.timeMode))
+		self.list.append(getConfigListEntry(_("Show red LED on VFD"), config.plugins.VFD_spark.redLed))
+		self.list.append(getConfigListEntry(_("Show green LED on VFD"), config.plugins.VFD_spark.greenLed))
 
 		self["config"].list = self.list
 		self["config"].l.setList(self.list)
@@ -257,6 +291,64 @@ class VFD_SPARK:
 		print "[VFD-SPARK] aborting"
 		config.misc.standbyCounter.addNotifier(standbyCounterChanged, initial_call = False)
 
+baseSessionGlobals__init__ = None
+SessionGlobals_instance = None
+
+def newSessionGlobals__init__(self, session):
+	baseSessionGlobals__init__(self, session)
+	global SessionGlobals_instance
+	SessionGlobals_instance = self
+	from Components.Sources.CurrentService import CurrentService
+	from Components.Sources.EventInfo import EventInfo
+	from Components.Sources.FrontendStatus import FrontendStatus
+	from Components.Sources.FrontendInfo import FrontendInfo
+	from Components.Sources.Source import Source
+	from Components.Sources.TunerInfo import TunerInfo
+	from Components.Sources.Boolean import Boolean
+	from Components.Sources.RecordState import RecordState
+	from Components.Converter.Combine import Combine
+	from Components.Renderer.FrontpanelLed import FrontpanelLed
+	self["CurrentService"] = CurrentService(session.nav)
+	self["Event_Now"] = EventInfo(session.nav, EventInfo.NOW)
+	self["Event_Next"] = EventInfo(session.nav, EventInfo.NEXT)
+	self["FrontendStatus"] = FrontendStatus(service_source = session.nav.getCurrentService)
+	self["FrontendInfo"] = FrontendInfo(navcore = session.nav)
+	self["VideoPicture"] = Source()
+	self["TunerInfo"] = TunerInfo()
+	self["RecordState"] = RecordState(session)
+	self["Standby"] = Boolean(fixed = False)
+
+	from Components.SystemInfo import SystemInfo
+
+	combine = Combine(func = lambda s: {(False, False): 0, (False, True): 1, (True, False): 2, (True, True): 3}[(s[0].boolean, s[1].boolean)])
+	combine.connect(self["Standby"])
+	combine.connect(self["RecordState"])
+
+	#                      |  two leds  | single led |
+	# recordstate  standby   red green
+	#    false      false    off   on     off
+	#    true       false    blnk  on     blnk
+	#    false      true      on   off    off
+	#    true       true     blnk  off    blnk
+
+	PATTERN_ON     = (20, 0xffffffff, 0xffffffff)
+	PATTERN_OFF    = (20, 0, 0)
+	PATTERN_BLINK  = (20, 0x55555555, 0xa7fccf7a)
+
+	nr_leds = SystemInfo.get("NumFrontpanelLEDs", 0)
+
+	if nr_leds == 1:
+		FrontpanelLed(which = 0, boolean = False, patterns = [PATTERN_OFF, PATTERN_BLINK, PATTERN_OFF, PATTERN_BLINK]).connect(combine)
+	elif nr_leds == 2:
+		if config.plugins.VFD_spark.redLed.value == '1':
+			FrontpanelLed(which = 0, boolean = False, patterns = [PATTERN_OFF, PATTERN_BLINK, PATTERN_ON, PATTERN_BLINK]).connect(combine)
+		if config.plugins.VFD_spark.greenLed.value == '1':
+			FrontpanelLed(which = 1, boolean = False, patterns = [PATTERN_OFF, PATTERN_OFF, PATTERN_ON, PATTERN_OFF]).connect(combine)
+		else:
+			FrontpanelLed(which = 0, boolean = False, patterns = [PATTERN_OFF, PATTERN_OFF, PATTERN_OFF, PATTERN_OFF]).connect(combine)
+			FrontpanelLed(which = 1, boolean = False, patterns = [PATTERN_OFF, PATTERN_OFF, PATTERN_OFF, PATTERN_OFF]).connect(combine)
+
+
 def main(menuid):
 	if menuid != "system":
 		return [ ]
@@ -293,6 +385,10 @@ def sessionstart(reason, **kwargs):
 	else:
 		gReason = reason
 	controlsparkVfd()
+	global baseSessionGlobals__init__	
+	if baseSessionGlobals__init__ is None:
+		baseSessionGlobals__init__ = SessionGlobals.__init__
+		SessionGlobals.__init__ = newSessionGlobals__init__
 
 def Plugins(**kwargs):
 		if getBoxType() in ('amiko8900', 'sognorevolution', 'arguspingulux', 'arguspinguluxmini', 'sparkreloaded', 'sabsolo', 'sparklx', 'gis8120'):
