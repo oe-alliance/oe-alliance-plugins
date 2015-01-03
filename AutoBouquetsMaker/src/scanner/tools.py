d8359d7a4d3644b76d1982239f1eeddd5c5d6826
Peter de Jonge <peter@jonge.nu>
2015-01-03 15:12:00 +0100

    [ABM]Custom radio LCN file added.

--- AutoBouquetsMaker/src/scanner/tools.py
+++ AutoBouquetsMaker/src/scanner/tools.py
@@ -25,7 +25,7 @@ class Tools():
 		custom_dir = os.path.dirname(__file__) + "/../custom"
 		skipextrachannels = 0
 
-		# Write Example custom file	
+		# Write Example TV custom file	
 		if current_bouquet_key.startswith('sd'):		
 			xmlout = open(custom_dir + "/EXAMPLE_sd_" + section_identifier + "_CustomLCN.xml", "w")
 		else:
@@ -41,14 +41,30 @@ class Tools():
 		xmlout.write("\t</lcnlist>\n</custom>\n")
 		xmlout.close()
 		
-		# Read custom file
+		# Write Example Radio custom file	
+		if current_bouquet_key.startswith('sd'):		
+			xmlout = open(custom_dir + "/EXAMPLE_sd_" + section_identifier + "_CustomradioLCN.xml", "w")
+		else:
+			xmlout = open(custom_dir + "/EXAMPLE_hd_" + section_identifier + "_CustomradioLCN.xml", "w")
+		xmlout.write("<custom>\n\t<include>yes</include>\n\t<lcnlist>\n")
+		for number in services["radio"]:
+			servicename = unicode(services["radio"][number]["service_name"], errors='ignore')
+			xmlout.write("\t\t<configuration lcn=\"%d\" channelnumber=\"%d\" description=\"%s\"></configuration>\n" % (
+				number,
+				number,
+				servicename.replace("&","+")
+				))
+		xmlout.write("\t</lcnlist>\n</custom>\n")
+		xmlout.close()
+		
+		# Read custom TV file
 		if current_bouquet_key.startswith('sd'):
 			customfile = custom_dir + "/sd_" + section_identifier + "_CustomLCN.xml"
 		else:
 			customfile = custom_dir + "/hd_" + section_identifier + "_CustomLCN.xml"
 		dom = self.parseXML(customfile)
 		if dom is None:
-			print>>log, "[Tools] No custom LCN file."
+			print>>log, "[Tools] No custom TV LCN file."
 		elif dom.documentElement.nodeType == dom.documentElement.ELEMENT_NODE and dom.documentElement.tagName == "custom":
 			j = 0
 			customlcndict = {}
@@ -80,7 +96,7 @@ class Tools():
 						oldservices = 1
 				if oldservices == 0:
 					newservices.append(number)
-			print>>log, "[Tools] New services %s" % (str(newservices))
+			print>>log, "[Tools] New TV services %s" % (str(newservices))
 			
 			lastlcn = 1
 			for number in customlcndict:
@@ -109,6 +125,74 @@ class Tools():
 					
 			services["video"] = video_services
 
+		# Read custom radio file
+		if current_bouquet_key.startswith('sd'):
+			customfile = custom_dir + "/sd_" + section_identifier + "_CustomradioLCN.xml"
+		else:
+			customfile = custom_dir + "/hd_" + section_identifier + "_CustomradioLCN.xml"
+		dom = self.parseXML(customfile)
+		if dom is None:
+			print>>log, "[Tools] No custom radio LCN file."
+		elif dom.documentElement.nodeType == dom.documentElement.ELEMENT_NODE and dom.documentElement.tagName == "custom":
+			j = 0
+			customlcndict = {}
+			for node in dom.documentElement.childNodes:
+				if node.nodeType != node.ELEMENT_NODE:
+					continue
+				if node.tagName == "include":
+					node.normalize()
+					if len(node.childNodes) == 1 and node.childNodes[0].nodeType == node.TEXT_NODE:
+						if node.childNodes[0].data.encode("utf-8") == 'no':
+							skipextrachannels = 1
+				if node.tagName == "lcnlist":
+					for node2 in node.childNodes:
+						if node2.nodeType == node2.ELEMENT_NODE and node2.tagName == "configuration":
+							customlcndict[j] = {}
+							for i in range(0, node2.attributes.length):
+								if node2.attributes.item(i).name == "lcn":
+									customlcndict[j]["lcn"] = int(node2.attributes.item(i).value)
+								elif node2.attributes.item(i).name == "channelnumber":
+									customlcndict[j]["channelnumber"] = int(node2.attributes.item(i).value)
+							j += 1
+			
+			# Find new services for log file
+			newservices = []
+			for number in services["radio"]:
+				oldservices = 0
+				for key in customlcndict:
+					if number == customlcndict[key]["channelnumber"]:
+						oldservices = 1
+				if oldservices == 0:
+					newservices.append(number)
+			print>>log, "[Tools] New radio services %s" % (str(newservices))
+			
+			lastlcn = 1
+			for number in customlcndict:
+				if customlcndict[number]["lcn"] > lastlcn:
+					lastlcn = customlcndict[number]["lcn"]
+				
+			# service radio swap.
+			radio_services = {}
+			radio_services_tmp = {}
+			radio_services_tmp = services["radio"]
+			radio_tmp ={}
+			for key in radio_services_tmp:
+				servicefound = 0
+				radio_tmp = radio_services_tmp[key]
+				number = radio_tmp["number"]
+				for key2 in customlcndict:
+					if number == customlcndict[key2]["channelnumber"]:
+						radio_tmp["logical_channel_number"] = customlcndict[key2]["lcn"]
+						radio_tmp["number"] = customlcndict[key2]["lcn"]
+						radio_services[customlcndict[key2]["lcn"]] = radio_tmp
+						servicefound = 1
+				# Service not in custom lcn file, add at end of list.
+				if servicefound == 0 and skipextrachannels == 0:
+					radio_services[lastlcn + 1] = radio_tmp
+					lastlcn += 1
+					
+			services["radio"] = radio_services
+			
 		return services
 		
 	def customtransponder(self, provider_key):
