e8e0d1d3c4547d858f4bc9576088fad8b4c1305f
arn354 <arn354@email.de>
2015-07-04 18:43:09 +0200

    Blindscan - plugin.py add FEC_4_5
    Traceback (most recent call last):
      File "/usr/lib/enigma2/python/Plugins/SystemPlugins/Blindscan/plugin.py", line 666, in blindscanContainerClose
    KeyError: 'FEC_4_5'
    
    16E Eutelsat

--- Blindscan/src/plugin.py
+++ Blindscan/src/plugin.py
@@ -639,6 +639,7 @@ class Blindscan(ConfigListScreen, Screen):
 						"FEC_1_2" : parm.FEC_1_2,
 						"FEC_2_3" : parm.FEC_2_3,
 						"FEC_3_4" : parm.FEC_3_4,
+						"FEC_4_5" : parm.FEC_4_5,
 						"FEC_5_6": parm.FEC_5_6,
 						"FEC_7_8" : parm.FEC_7_8,
 						"FEC_8_9" : parm.FEC_8_9,
