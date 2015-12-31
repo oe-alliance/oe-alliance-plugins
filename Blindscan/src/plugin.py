414bdb4e862eaf5d100c684da3ba56ca168be47e
Huevos <rowland@moralet.com>
2015-12-31 02:43:46 +0100

    [Blindscan] fix potential BSoD when sat list is empty.

--- Blindscan/src/plugin.py
+++ Blindscan/src/plugin.py
@@ -284,7 +284,7 @@ class Blindscan(ConfigListScreen, Screen):
 		for n in nimmanager.nim_slots:
 			if n.config_mode == "nothing":
 				continue
-			if n.config_mode == "advanced" and len(nimmanager.getSatListForNim(n.slot)) < 1:
+			if n.isCompatible("DVB-S") and len(nimmanager.getSatListForNim(n.slot)) < 1:
 				continue
 			if n.config_mode in ("loopthrough", "satposdepends"):
 				root_id = nimmanager.sec.getRoot(n.slot_id, int(n.config.connectedTo.value))
