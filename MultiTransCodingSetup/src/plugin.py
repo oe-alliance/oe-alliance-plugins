c77d333e060d9a5b69181d3c7e2098d3ecfe701c
captain <openatv@gmail.com>
2018-01-30 22:24:10 +0100

    [multitrancoding] add vcodec support

--- MultiTransCodingSetup/src/plugin.py
+++ MultiTransCodingSetup/src/plugin.py
@@ -12,12 +12,20 @@ from Components.Sources.StaticText import StaticText
 from Plugins.Plugin import PluginDescriptor
 from enigma import eTimer
 from boxbranding import getMachineBuild
+from Tools.Directories import fileExists, pathExists
 from os import path
 
 
 config.plugins.transcodingsetup = ConfigSubsection()
 config.plugins.transcodingsetup.transcoding = ConfigSelection(default = "enable", choices = [ ("enable", _("enable")), ("disable", _("disable"))])
-if getMachineBuild() in ('ew7356','formuler1','formuler1tc'):
+if fileExists("/proc/stb/encoder/0/vcodec"):
+	config.plugins.transcodingsetup.bitrate = ConfigSelection([("100000", _("100 kbps")), ("300000", _("300 kbps")), ("500000", _("500 kbps")), ("800000", _("800 kbps")), ("1000000", _("1.0 Mbps")),  ("1200000", _("1.2 Mbps")), ("1500000", _("1.5 Mbps")), ("2000000", _("2.0 Mbps")), ("2500000", _("2.5 Mbps")), ("3000000", _("3.0 Mbps")), ("3500000", _("3.5 Mbps")), ("4000000", _("4.0 Mbps")), ("5000000", _("5.0 Mbps"))], default="1500000")
+	config.plugins.transcodingsetup.resolution = ConfigSelection([("720x480", _("480p")), ("720x576", _("576p")), ("1280x720", _("720p"))], default="720x576")
+	config.plugins.transcodingsetup.vcodec = ConfigSelection([("h264", _("H.264")), ("h265", _("H.265"))], default="h265")
+	config.plugins.transcodingsetup.framerate = ConfigSelection([("23976", _("23.976 fps")), ("24000", _("24 fps")), ("25000", _("25 fps")), ("30000", _("30 fps"))], default="25000")
+	config.plugins.transcodingsetup.aspectratio = ConfigInteger(default = 2)
+	config.plugins.transcodingsetup.interlaced = ConfigInteger(default = 0)
+elif getMachineBuild() in ('ew7356','formuler1','formuler1tc'):
 	choice = ConfigSelection(default = "400000", choices=[("-1", "Auto"), ("50000", "50 Kbits"), ("100000", "100 Kbits"), ("150000", "150 Kbits"), ("200000", "200 Kbits"), ("250000", "250 Kbits"), ("300000", "300 Kbits"), ("350000", "350 Kbits"), ("400000", "400 Kbits"), ("450000", "450 Kbits"), ("500000", "500 Kbits"), ("600000", "600 Kbits"), ("700000", "700 Kbits"), ("800000", "800 Kbits"), ("900000", "900 Kbits"), ("1000000", "1 Mbits")])
 	config.plugins.transcodingsetup.bitrate = choice
 	choice = ConfigSelection(default = "50000", choices = [("-1", "Auto"), ("23976", "23.976 fps"), ("24000", "24 fps"), ("25000", "25 fps"), ("29970", "29.970 fps"), ("30000", "30 fps"), ("50000", "50 fps"), ("59940", "59.940 fps"), ("60000", "60 fps")])
@@ -78,9 +86,12 @@ class TranscodingSetup(Screen,ConfigListScreen):
 
 		text =  (_("To use transcoding You can build URL for VLC by Yourself\n\n"))
 		self["HelpTextHeader1"] = Label(text)
-
-		text2 = "http://STB_IP:PORT/CH_REF:?bitrate=BITRATE?width=WIDTH?height=HEIGHT?aspectration=ASPECT?interlaced=0\n\ne.x:\n\n"
-		text2 += "http://192.168.1.5:8001/1:0:1:C25:1E78:71:820000:0:0:0:?bitrate=300000?width=320?height=240?aspectratio=2?interlaced=0\n\n"
+		if fileExists("/proc/stb/encoder/0/vcodec"):
+			text2 = "http://STB_IP:PORT/CH_REF:?bitrate=BITRATE?width=WIDTH?height=HEIGHT?vcodec=VCODEC?aspectration=ASPECT?interlaced=0\n\ne.x:\n\n"
+			text2 += "http://192.168.1.5:8001/1:0:1:C25:1E78:71:820000:0:0:0:?bitrate=300000?width=320?height=240?vcodec=h265?aspectratio=2?interlaced=0\n\n"
+		else:
+			text2 = "http://STB_IP:PORT/CH_REF:?bitrate=BITRATE?width=WIDTH?height=HEIGHT?aspectration=ASPECT?interlaced=0\n\ne.x:\n\n"
+			text2 += "http://192.168.1.5:8001/1:0:1:C25:1E78:71:820000:0:0:0:?bitrate=300000?width=320?height=240?aspectratio=2?interlaced=0\n\n"
 		self["HelpText1"] = Label(_(text2))
 
 		text3 = (_("Transcoding from HDMI-IN e.x:\n\n"))
@@ -105,7 +116,14 @@ class TranscodingSetup(Screen,ConfigListScreen):
 
 	def createSetup(self):
 		self.list = []
-		if getMachineBuild() in ('ew7356','formuler1','formuler1tc'):
+		if fileExists("/proc/stb/encoder/0/vcodec"):
+			self.list.append(getConfigListEntry(_("Bitrate in bits"), config.plugins.transcodingsetup.bitrate))
+			self.list.append(getConfigListEntry(_("Framerate"), config.plugins.transcodingsetup.framerate))
+			self.list.append(getConfigListEntry(_("Resolution"), config.plugins.transcodingsetup.resolution))
+			self.list.append(getConfigListEntry(_("Video codec"), config.plugins.transcodingsetup.vcodec))
+			self.list.append(getConfigListEntry(_("Aspect Ratio"), config.plugins.transcodingsetup.aspectratio))
+			self.list.append(getConfigListEntry(_("Interlaced"), config.plugins.transcodingsetup.interlaced))
+		elif getMachineBuild() in ('ew7356','formuler1','formuler1tc'):
 			self.list.append(getConfigListEntry(_("Bitrate in bits"), config.plugins.transcodingsetup.bitrate))
 			self.list.append(getConfigListEntry(_("Framerate"), config.plugins.transcodingsetup.framerate))
 		else:
