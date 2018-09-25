35400404d5e509eedbdbbfba7707b3da33deb8b2
Captain <captain.onboard@web.de>
2018-09-25 16:01:01 +0200

    add ustym4kpro

--- Blindscan/src/plugin.py
+++ Blindscan/src/plugin.py
@@ -834,6 +834,9 @@ class Blindscan(ConfigListScreen, Screen):
 		elif getBrandOEM() == 'clap':
 			self.frontend and self.frontend.closeFrontend()
 			cmd = "clap-blindscan %d %d %d %d %d %d %d %d %d %d" % (temp_start_int_freq, temp_end_int_freq, self.blindscan_start_symbol.value, self.blindscan_stop_symbol.value, tab_pol[pol], tab_hilow[band], self.feid, self.getNimSocket(self.feid), self.is_c_band_scan,orb[0])
+		elif getBrandOEM() == 'uclan':
+			self.frontend and self.frontend.closeFrontend()
+			cmd = "uclan-blindscan %d %d %d %d %d %d %d %d %d %d" % (temp_start_int_freq, temp_end_int_freq, self.blindscan_start_symbol.value, self.blindscan_stop_symbol.value, tab_pol[pol], tab_hilow[band], self.feid, self.getNimSocket(self.feid), self.is_c_band_scan,orb[0])
 		elif getBoxType() == 'sf8008':
 			self.frontend and self.frontend.closeFrontend()
 			cmd = "octagon-blindscan %d %d %d %d %d %d %d %d %d %d" % (temp_start_int_freq, temp_end_int_freq, self.blindscan_start_symbol.value, self.blindscan_stop_symbol.value, tab_pol[pol], tab_hilow[band], self.feid, self.getNimSocket(self.feid), self.is_c_band_scan,orb[0])
