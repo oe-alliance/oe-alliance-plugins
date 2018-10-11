911ae4c4a7b247899e7274b90d0cb82a0c1b906f
Huevos <rowland@moralet.com>
2018-10-11 09:25:07 +0200

    [TerrestrialScan] Add channel names for AUS
    Credit: PRL

--- TerrestrialScan/src/TerrestrialScan.py
+++ TerrestrialScan/src/TerrestrialScan.py
@@ -60,6 +60,15 @@ def getChannelNumber(frequency, descr):
 		elif 470 <= f < 863: 	# IV,V
 			d = (f + 2) % 8
 			return str(int(f - 470) / 8 + 21) + (d < 3.5 and "-" or d > 4.5 and "+" or "")
+	elif descr == "australia":
+		if 174 < f < 202:	 # III: CH6-CH9
+			return str(int(f - 174) / 7 + 6)
+		elif 202 <= f < 209:	 # III: CH9A
+			return "9A"
+		elif 209 <= f < 230:	 # III: CH10-CH12
+			return str(int(f - 209) / 7 + 10)
+		elif 526 < f < 820:	 # IV, V: CH28-CH69
+			return str(int(f - 526) / 7 + 28)
 	return ""
 
 class TerrestrialScan(Screen):
