1cfd9fef37b2298920d5b152c39188a8c9ead65d
persianpros <persianpros@yahoo.com>
2019-02-05 14:19:59 +0330

    Update all po files with latest strings

--- updateallpo.sh
+++ updateallpo.sh
@@ -3,7 +3,7 @@
 #  
 # Pre-requisite:
 # The following tools must be installed on your system and accessible from path
-# gawk, find, xgettext, gsed, python, msguniq, msgmerge, msgattrib, msgfmt, msginit
+# gawk, find, xgettext, sed, python, msguniq, msgmerge, msgattrib, msgfmt, msginit
 #
 # Run this script from the top folder of enigma2-plugins
 #
@@ -18,13 +18,13 @@ rootpath=$PWD
 printf "Po files update/creation from script starting.\n"
 for directory in */po/ ; do
 	cd $rootpath/$directory
-	languages=($(ls *.po | tr "\n" " " | gsed 's/.po//g'))
+	languages=($(ls *.po | tr "\n" " " | sed 's/.po//g'))
 	plugin=$(gawk ' BEGIN { FS=" " } /^PLUGIN/ { print $3 }' Makefile.am)
 	printf "Processing plugin %s\n" $plugin
 	#
 	printf "Creating temporary file $plugin-py.pot\n"
 	find -s -X .. -name "*.py" -exec xgettext --no-wrap -L Python --from-code=UTF-8 -kpgettext:1c,2 --add-comments="TRANSLATORS:" -d $plugin -s -o $plugin-py.pot {} \+
-	gsed --in-place $plugin-py.pot --expression=s/CHARSET/UTF-8/
+	sed --in-place $plugin-py.pot --expression=s/CHARSET/UTF-8/
 	printf "Creating temporary file $plugin-xml.pot\n"
 	find -s -X .. -name "*.xml" -exec python $rootpath/xml2po.py {} \+ > $plugin-xml.pot
 	printf "Merging pot files to create: %s.pot\n" $plugin
