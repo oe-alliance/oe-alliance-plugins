18d22bc5c9edd66b5d6af2821c211aba32b8d223
Huevos <rowland@moralet.com>
2020-12-02 20:15:55 +0100

    [AboutBoxBranding] write output to debug log on enigma start

--- AboutBoxBranding/src/plugin.py
+++ AboutBoxBranding/src/plugin.py
@@ -15,7 +15,7 @@ class AboutBoxBranding(Screen):
 		self.skinName = ["AboutBoxBranding", "AboutOE", "About"]
 
 		self["key_red"] = StaticText(_("Close"))
-		self["AboutScrollLabel"] = ScrollLabel(self.getBoxbranding())
+		self["AboutScrollLabel"] = ScrollLabel(getBoxbranding())
 
 		self["actions"] = ActionMap(["WizardActions", "ColorActions"],
 		{
@@ -33,31 +33,33 @@ class AboutBoxBranding(Screen):
 	def pageDown(self):
 		self["AboutScrollLabel"].pageDown()
 
-	def getBoxbranding(self):
-		import boxbranding
-		bblist = []
-		for m in sorted(boxbranding.__dict__.keys()):
-			if callable(getattr(boxbranding, m)):
-				v = getattr(boxbranding, m)()
-				for x in ("http://", "https://"): # Trim URLs to domain only
-					if v.startswith(x):
-						v = x + v.split(x)[1].split('/')[0] + "/"
-				bblist.append("%s:\t%s\n" % (m, v))
-		return ''.join(bblist)
+def getBoxbranding():
+	import boxbranding
+	bblist = []
+	for m in sorted(boxbranding.__dict__.keys()):
+		if callable(getattr(boxbranding, m)):
+			v = getattr(boxbranding, m)()
+			for x in ("http://", "https://"): # Trim URLs to domain only
+				if v.startswith(x):
+					v = v.split(x)[1].split('/')[0] + " [...]"
+					break
+			bblist.append("%s:\t%s\n" % (m, v))
+	return ''.join(bblist)
 
 def main(session, **kwargs):
 	session.open(AboutBoxBranding)
 
 def start(menuid):
-	try:
-		import boxbranding
-		if menuid == "information":
-			return [(_("Boxbranding"), main, "About Boxbranding", 1000)]
-	except:
-		print("[AboutBoxBranding] Error: %s: '%s'!" % (type(err).__name__, err))
+	if menuid == "information":
+		return [(_("Boxbranding"), main, "About Boxbranding", 1000)]
 	return []
 
 def Plugins(**kwargs):
+	try:
+		print("[AboutBoxBranding]...\n%s" % getBoxbranding())
+	except:
+		print("[AboutBoxBranding] Error: %s: '%s'!" % (type(err).__name__, err))
+		return []
 	from Plugins.Plugin import PluginDescriptor
 	return [PluginDescriptor(where = PluginDescriptor.WHERE_MENU, fnc=start)]
 			
