75fda8af079119a016020fbc7b374b888bb62c26
Huevos <rowland@moralet.com>
2020-12-08 16:21:57 +0100

    [AboutBoxBranding] format print out; add CH+/-
    Print format changes by Twol

--- AboutBoxBranding/src/plugin.py
+++ AboutBoxBranding/src/plugin.py
@@ -1,4 +1,5 @@
-# for localized messages
+from __future__ import print_function
+
 from . import _
 
 from Components.ActionMap import ActionMap
@@ -15,16 +16,17 @@ class AboutBoxBranding(Screen):
 		self.skinName = ["AboutBoxBranding", "AboutOE", "About"]
 
 		self["key_red"] = StaticText(_("Close"))
-		self["AboutScrollLabel"] = ScrollLabel(getBoxbranding())
+		self["AboutScrollLabel"] = ScrollLabel(getBoxbranding()[0])
 
-		self["actions"] = ActionMap(["WizardActions", "ColorActions"],
+		self["actions"] = ActionMap(["SetupActions","NavigationActions"],
 		{
-			"back": self.close,
-			"red": self.close,
+			"cancel": self.close,
 			"up": self.pageUp,
 			"down":	self.pageDown,
 			"left":	self.pageUp,
 			"right": self.pageDown,
+			"pageUp": self.pageUp,
+			"pageDown": self.pageDown,
 		}, -2)
 
 	def pageUp(self):
@@ -36,6 +38,7 @@ class AboutBoxBranding(Screen):
 def getBoxbranding():
 	import boxbranding
 	bblist = []
+	prlist = []
 	for m in sorted(boxbranding.__dict__.keys()):
 		if callable(getattr(boxbranding, m)):
 			v = getattr(boxbranding, m)()
@@ -43,8 +46,9 @@ def getBoxbranding():
 				if v.startswith(x):
 					v = v.split(x)[1].split('/')[0] + " [...]"
 					break
-			bblist.append("%s:\t%s\n" % (m, v))
-	return ''.join(bblist)
+			bblist.append("%s:\t %s\n" % (m, v))
+			prlist.append(tuple([m, v]))
+	return (''.join(bblist), prlist)
 
 def main(session, **kwargs):
 	session.open(AboutBoxBranding)
@@ -55,9 +59,14 @@ def start(menuid):
 	return []
 
 def Plugins(**kwargs):
+	prlist = []
 	try:
-		print("[AboutBoxBranding]...\n%s" % getBoxbranding())
-	except:
+		prlist = getBoxbranding()[1]
+		res = len(prlist) 
+		print("[AboutBoxBranding]... Number of entries = %s" % (res))
+		for x in range(0, res):
+			print("{: <24} {: <20}".format(prlist[x][0], prlist[x][1]))
+	except Exception as err:
 		print("[AboutBoxBranding] Error: %s: '%s'!" % (type(err).__name__, err))
 		return []
 	from Plugins.Plugin import PluginDescriptor
