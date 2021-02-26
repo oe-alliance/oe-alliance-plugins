e26b8df74eda50c56cc4d03478f803cd03346126
Huevos <rowland@moralet.com>
2021-02-26 16:00:02 +0100

    [EPGTranslator] fix inconsistent use of tabs in indentation

--- EPGTranslator/src/plugin.py
+++ EPGTranslator/src/plugin.py
@@ -517,8 +517,8 @@ class translatorConfig(ConfigListScreen, Screen):
 # ==================================================================
 
     def selectionChanged(self):
-		self["description"].setText(self.getCurrentDescription())
-		
+        self["description"].setText(self.getCurrentDescription())
+
 # ==================================================================
     def UpdateComponents(self):
         png = lang_flag(str(CfgPlTr.destination.getValue()))
