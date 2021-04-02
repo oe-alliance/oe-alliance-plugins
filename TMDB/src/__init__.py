2a4aad3718ad2e417ab92e1891790c64727eabe9
Jörg Bleyel <jbleyel@gmx.net>
2021-04-02 14:17:10 +0200

    Add TMDB

---
+++ TMDB/src/__init__.py
@@ -0,0 +1,24 @@
+﻿# -*- coding: utf-8 -*-
+
+from Components.Language import language
+from Tools.Directories import resolveFilename, SCOPE_PLUGINS
+import os, gettext
+
+PluginLanguageDomain = "tmdb"
+PluginLanguagePath = "Extensions/tmdb/locale"
+
+def localeInit():
+	lang = language.getLanguage()[:2]
+	os.environ["LANGUAGE"] = lang
+	print "[TMDb] set language to ", lang
+	gettext.bindtextdomain(PluginLanguageDomain, resolveFilename(SCOPE_PLUGINS, PluginLanguagePath))
+
+def _(txt):
+	t = gettext.dgettext(PluginLanguageDomain, txt)
+	if t == txt:
+		print "[TMDb] fallback to default Enigma2 Translation for", txt
+		t = gettext.gettext(txt)
+	return t
+
+localeInit()
+language.addCallback(localeInit)
---
+++ TMDB/src/plugin.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#######################################################################
+# maintainer: <schomi@vuplus-support.org> 
+# This plugin is free software, you are allowed to
+# modify it (if you keep the license),
+# but you are not allowed to distribute/publish
+# it without source code (this version and your modifications).
+# This means you also have to distribute
+# source code of your modifications.
+#######################################################################
+
+from Plugins.Plugin import PluginDescriptor
+import tmdb
+from __init__ import _
+
+def main(session, service, **kwargs):
+	reload(tmdb)
+	try:
+		session.open(tmdb.tmdbScreen, service, 1)
+	except:
+		import traceback
+		traceback.print_exc()
+		
+def eventinfo(session, eventName="", **kwargs):
+	reload(tmdb)
+	try:
+		s = session.nav.getCurrentService()
+		info = s.info()
+		event = info.getEvent(0) # 0 = now, 1 = next
+		name = event and event.getEventName() or ''
+		session.open(tmdb.tmdbScreen, name, 2)
+	except:
+		import traceback
+		traceback.print_exc()
+		
+def Plugins(**kwargs):
+	return [
+			PluginDescriptor(name=_("TMDb"), description=_("TMDb Infos ..."), where = PluginDescriptor.WHERE_MOVIELIST, fnc=main, needsRestart = False),
+			PluginDescriptor(name=_("TMDb") ,description=_("TMDb Infos ..."), where=PluginDescriptor.WHERE_EVENTINFO, fnc=eventinfo, needsRestart = False)
+			]
