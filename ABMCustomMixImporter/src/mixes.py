55e489a0d96b85f0dc6acce21d05257074e22d58
davesayers2014 <davesayers2012@gmail.com>
2021-06-01 00:18:20 +0100

    ABM CustomMix Importer py3 conversion
    Thanks @KiddaC

--- ABMCustomMixImporter/src/mixes.py
+++ ABMCustomMixImporter/src/mixes.py
@@ -8,14 +8,14 @@ class Mixes():
 	def parseXML(self, filename):
 		try:
 			mix = open(filename, "r")
-		except Exception, e:
-			print "[ABMCustomMixImporter][Mixes] Cannot open %s: %s" % (filename, e)
+		except Exception as e:
+			print("[ABMCustomMixImporter][Mixes] Cannot open %s: %s" % (filename, e))
 			return None
 
 		try:
 			dom = xml.dom.minidom.parse(mix)
-		except Exception, e:
-			print "[ABMCustomMixImporter][Mixes] XML parse error (%s): %s" % (filename, e)
+		except Exception as e:
+			print("[ABMCustomMixImporter][Mixes] XML parse error (%s): %s" % (filename, e))
 			mix.close()
 			return None
 
@@ -42,19 +42,22 @@ class Mixes():
 					if node.tagName == "name":
 						node.normalize()
 						if len(node.childNodes) == 1 and node.childNodes[0].nodeType == node.TEXT_NODE:
-							mix["name"] = node.childNodes[0].data.encode("utf-8")
+							# mix["name"] = node.childNodes[0].data.encode("utf-8")
+							mix["name"] = node.childNodes[0].data
 					elif node.tagName == "provider":
 						node.normalize()
 						if len(node.childNodes) == 1 and node.childNodes[0].nodeType == node.TEXT_NODE:
-							mix["provider"] = node.childNodes[0].data.encode("utf-8")
+							# mix["provider"] = node.childNodes[0].data.encode("utf-8")
+							mix["provider"] = node.childNodes[0].data
 					elif node.tagName == "url":
 						node.normalize()
 						if len(node.childNodes) == 1 and node.childNodes[0].nodeType == node.TEXT_NODE:
-							mix["url"] = node.childNodes[0].data.encode("utf-8")
+							# mix["url"] = node.childNodes[0].data.encode("utf-8")
+							mix["url"] = node.childNodes[0].data
 
 			if not ("name" in mix and "provider" in mix and "url" in mix):
 
-				print "[ABMCustomMixImporter][Mixes] Incomplete XML %s" % filename
+				print("[ABMCustomMixImporter][Mixes] Incomplete XML %s" % filename)
 				continue
 
 			mixes[mix["key"]] = mix
