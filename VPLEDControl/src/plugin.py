a8192473239d348204eda13f62e229109f6cf050
Captain <captain.onboard@web.de>
2021-08-21 11:06:49 +0200

    add 8008ta

--- VPLEDControl/src/plugin.py
+++ VPLEDControl/src/plugin.py
@@ -33,7 +33,7 @@ config.plugins.VFD_ini.ClockLevel2 = ConfigSlider(default=4, limits=(1, 10))
 
 MyRecLed = False
 use_oled = False
-if HardwareInfo().get_device_model() in ("sf8008", "sf8008m", "gbmv200", "viper4k"):
+if HardwareInfo().get_device_model() in ("sf8008", "sf8008m", "sf8008ta", "gbmv200", "viper4k"):
 	use_oled = True
 
 
