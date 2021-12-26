2a70c2e9949443dfa39c23cd05eb59ebb71f775f
dpeddi <eddi@depieri.net>
2021-12-26 23:24:05 +0100

    fix "[browser] can't concat str to bytes"

--- HbbTV/src/plugin.py
+++ HbbTV/src/plugin.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 import os
 import struct
+import six
 from Plugins.Plugin import PluginDescriptor
 from Screens.Screen import Screen
 from Screens.InfoBar import InfoBar
@@ -178,7 +179,11 @@ class VBHandler(VBHandlers):
 	def _CB_OOIF_GET_CURRENT_CHANNEL(self, result, packet):
 		if (vbcfg.g_channel_info):
 			try:
-				data = struct.pack('iiii', vbcfg.g_channel_info[0], vbcfg.g_channel_info[1], vbcfg.g_channel_info[2], len(vbcfg.g_channel_info[3])) + vbcfg.g_channel_info[3]
+				data = struct.pack('iiii', vbcfg.g_channel_info[0], vbcfg.g_channel_info[1], vbcfg.g_channel_info[2], len(vbcfg.g_channel_info[3]))
+				if six.PY3:
+					data += bytes(vbcfg.g_channel_info[3], 'utf-8')
+				else:
+					data += vbcfg.g_channel_info[3]
 			except Exception as err:
 				vbcfg.ERR(err)
 				return (False, None)
