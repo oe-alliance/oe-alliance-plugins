a7cb047c18a64060c9f9569a5d50a6dd30d7f0b0
Mr.Servo <mrservo@gmx.de>
2022-11-07 12:21:07 +0100

    [Piconsupdater] changeover to twisted.internet.reactor.callInThread
    - imports cleaned up
    - DEPRECATED 'twisted.web.client.downloadPage & .getPage' replaced by current 'twisted.internet.reactor.callInThread'

--- PiconsUpdater/src/DownloadJob.py
+++ PiconsUpdater/src/DownloadJob.py
@@ -1,75 +1,84 @@
 # -*- coding: utf-8 -*-
-import os
-import six
-from six.moves.urllib.parse import urlparse
-from threading import Thread
-from twisted.web import client
-from twisted.web.client import HTTPDownloader
-from twisted.internet import reactor, ssl
+from os import path
+from requests import get, exceptions
+from twisted.internet.reactor import callInThread
+from six import ensure_binary
+from six.moves.urllib.parse import quote_plus
 from . import _, printToConsole
 
 
 class download:
-    def __init__(self, url, outputfile):
-        self.url = six.ensure_binary(url)
-        self.file = outputfile
+	def __init__(self, url, outputfile):
+		self.url = url
+		self.file = outputfile
 
-    def start(self):
-        return client.downloadPage(self.url, self.file)
+	def start(self, success, fail=None):
+		return callInThread(self.threadDownloadPage, self.url, self.file, success, fail)
 
-    def stop(self):
-        return
+	def stop(self):
+		return
+
+	def threadDownloadPage(self, link, file, success, fail=None):
+		link = ensure_binary(quote_plus(link))
+		try:
+			response = get(link)
+			response.raise_for_status()
+			with open(file, "wb") as f:
+				f.write(response.content)
+			success(file)
+		except exceptions.RequestException as err:
+			if fail is not None:
+				fail(err)
 
 
 class DownloadJob:
 
-    def __init__(self, downloadUrl, targetFileName, callbackFinished=None, callbackFailed=None, override=False):
-        self.downloadUrl = downloadUrl
-        self.targetFileName = targetFileName
-        self.callbackFinished = callbackFinished
-        self.callbackFailed = callbackFailed
-        self.download = None
-        if override == False and os.path.isfile(self.targetFileName) is True:
-            t = Thread(target=self.__downloadFromCache)
-            t.start()
-        else:
-            self.run()
+	def __init__(self, downloadUrl, targetFileName, callbackFinished=None, callbackFailed=None, override=False):
+		self.downloadUrl = downloadUrl
+		self.targetFileName = targetFileName
+		self.callbackFinished = callbackFinished
+		self.callbackFailed = callbackFailed
+		self.download = None
+		if override == False and path.isfile(self.targetFileName) is True:
+			callInThread(self.__downloadFromCache)
+		else:
+			self.run()
 
-    def __del__(self):
-        self.clean()
+	def __del__(self):
+		self.clean()
 
-    def clean(self):
-        try:
-            del self.callbackFailed
-            del self.callbackFinished
-        except:
-            pass
+	def clean(self):
+		try:
+			del self.callbackFailed
+			del self.callbackFinished
+		except:
+			pass
 
-    def run(self):
-        self.download = download(self.downloadUrl, self.targetFileName)
-        self.download.start().addCallback(self.__downloadFinished).addErrback(self.__downloadFailed)
+	def run(self):
+		self.download = download(self.downloadUrl, self.targetFileName)
+		self.download.start(self.__downloadFinished, self.__downloadFailed)
 
-    def __downloadFromCache(self):
-        printToConsole("file '%s' already exists." % self.targetFileName)
-        self.__downloadFinished()
+	def __downloadFromCache(self):
+		printToConsole("file '%s' already exists." % self.targetFileName)
+		self.__downloadFinished()
 
-    def __downloadFinished(self, string=''):
-        if self.callbackFinished is not None:
-            callback = self.callbackFinished
-            self.clean()
-            callback(self)
-        if self.download:
-            self.download.stop()
-            self.download = None
+	def __downloadFinished(self, string=''):
+		if self.callbackFinished is not None:
+			callback = self.callbackFinished
+			self.clean()
+			callback(self)
+		if self.download:
+			self.download.stop()
+			self.download = None
 
-    def __downloadFailed(self, failureInstance=None, errorMessage=''):
-        self.errorMessage = errorMessage
-        if errorMessage == '' and failureInstance is not None:
-            self.errorMessage = failureInstance.getErrorMessage()
-        self.errorMessage = self.downloadUrl + ' - ' + self.errorMessage
-        if self.callbackFailed is not None:
-            callback = self.callbackFailed
-            self.clean()
-            callback(self)
-        self.download.stop()
-        self.download = None
+	def __downloadFailed(self, errorMessage=''):
+		self.errorMessage = errorMessage
+#		if errorMessage == '' and failureInstance is not None:
+#			self.errorMessage = failureInstance.getErrorMessage()
+#		self.errorMessage = self.downloadUrl + ' - ' + self.errorMessage
+		if self.callbackFailed is not None:
+			callback = self.callbackFailed
+			self.clean()
+			callback(self)
+		self.download.stop()
+		self.download = None
