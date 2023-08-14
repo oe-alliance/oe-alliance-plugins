b4475d5d3ce110670f3753b37abf1da11de9c2ae
Mr.Servo <mrservo@gmx.de>
2023-08-14 17:15:46 +0200

    [Chefkoch] & [TVSpielfilm] AGENTS-list bugfixed (#746)
    * [Chefkoch] v2.1 AGENTS-list bugfixed
    
    * [TVSpielfilm] V6.9 AGENTS-list bugfixed

--- Chefkoch/src/plugin.py
+++ Chefkoch/src/plugin.py
@@ -49,9 +49,9 @@ PICFILE = '/tmp/chefkoch.jpg'
 AGENTS = [
 		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36",
 		"Mozilla/5.0 (iPhone; CPU iPhone OS 14_4_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/604.1",
-		"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/110.0"
-		"Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 6.1)"
-		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36 Edge/87.0.664.75"
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/110.0",
+		"Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 6.1)",
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36 Edge/87.0.664.75",
 		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.18363"
 		]
 
