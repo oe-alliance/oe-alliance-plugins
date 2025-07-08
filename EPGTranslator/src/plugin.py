18fcb68af20c05c2a493a1ac167cfe4c801906b6
Orlandoxx <95180049+Orlandoxx@users.noreply.github.com>
2025-07-08 12:07:47 +0300

    Update plugin.py
    Fixed one localization ("Original").

--- EPGTranslator/src/plugin.py
+++ EPGTranslator/src/plugin.py
@@ -703,7 +703,7 @@ Red: Refresh EPG
     def onLayoutFinished(self):
         source = CfgPlTr.source.getValue()
         if source == "auto":
-            source = "Original"
+            source = _("Original")
         if self.showsource == 'yes':
             self['from_lang'].setText(source)
             dest_w = 'to_lang'
